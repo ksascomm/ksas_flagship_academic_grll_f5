02058af74a2ae3f9c6e89eb12ac1680361675806
ksascomm <ksasweb@jhu.edu>
2015-10-28 13:15:29 -0400

    spring & intermission '16 courses

--- template-courses-all.php
+++ template-courses-all.php
@@ -12,9 +12,9 @@ Template Name: ISIS Courses (All GRLL Depts)
 		$department_unclean = $theme_option['flagship_sub_isis_name'];
 		$department = str_replace(' ', '%20', $department_unclean);
 		$department = str_replace('&', '%26', $department);
-		$fall = 'fall%202014';
-		$spring = 'spring%202015';
-		$intersession = 'intersession%202015';
+		$fall = 'fall%202015';
+		$spring = 'spring%202016';
+		$intersession = 'intersession%202016';
 		$approval = 'approval%20required';
 		$closed = 'closed';
 		$open = 'open';
@@ -105,9 +105,9 @@ Template Name: ISIS Courses (All GRLL Depts)
 				<fieldset class="radius10">
 							<div class="row filter option-set" data-filter-group="term">
 									<div class="button radio"><a href="#" data-filter="" class="selected">View All</a></div>
-									<div class="button radio"><a href="#" data-filter=".Fall">Fall 2014 Courses</a></div>
-									<div class="button radio"><a href="#" data-filter=".Intersession">Intersession 2015 Courses</a></div>
-									<div class="button radio"><a href="#" data-filter=".Spring">Spring 2015 Courses</a></div>
+									<div class="button radio"><a href="#" data-filter=".Fall">Fall 2015 Courses</a></div>
+									<div class="button radio"><a href="#" data-filter=".Intersession">Intersession 2016 Courses</a></div>
+									<div class="button radio"><a href="#" data-filter=".Spring">Spring 2016 Courses</a></div>
 									<h5 class="inline"><a href="#" class="acc_expandall">[Expand All]</a></h5>
 
 							</div>
