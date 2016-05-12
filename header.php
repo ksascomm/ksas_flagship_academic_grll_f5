595f5e51eddc2706b5043600eccea12c51d690ea
ksascomm <ksasweb@jhu.edu>
2016-05-12 09:19:03 -0400

    modified date to header

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="date" content="<?php the_modified_date(); ?>" />
   <title><?php create_page_title(); ?></title>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" />
   <!-- CSS Files: All pages -->
