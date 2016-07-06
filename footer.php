7aec452a4a998a8cb71f72a089d08caa40499828
ksascomm <ksasweb@jhu.edu>
2016-07-06 15:23:04 -0400

    moved <nav> out of wp_nav_menu query

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
-  <footer>
-  	<div class="row hide-for-print" role="navigation" aria-label="Quicklinks Menu">
+<footer>
+  	<div class="row hide-for-print">
+		<nav class="small-10 medium-3 columns" id="quicklinks" aria-label="Quicklinks Menu">
   		<?php //Check Theme Options for Quicklinks setting
 	  		$theme_option = flagship_sub_get_global_options();
 	  		if ( $theme_option['flagship_sub_quicklinks']  == '1' ) {
@@ -7,22 +8,20 @@
 	  				$quicklinks_id = $theme_option['flagship_sub_quicklinks_id'];
 	  				switch_to_blog($quicklinks_id); }
 
-	  		//Quicklinks Menu
-	  		wp_nav_menu( array(
-				'theme_location' => 'quick_links',
-				'menu_class' => 'nav-bar',
-				'fallback_cb' => 'foundation_page_menu',
-				'container' => 'div',
-				'container_id' => 'quicklinks',
-				'container_class' => 'medium-3 columns hide-for-small',
-				'walker' => new foundation_navigation() ) );
+				//Quicklinks Menu
+		  		wp_nav_menu( array( 
+					'theme_location' => 'quick_links', 
+					'menu_class' => 'nav-bar', 
+					'fallback_cb' => 'foundation_page_menu', 
+					'container' => 'false', 
+					'walker' => new foundation_navigation() ) ); 
 
 			//Return to current site
 			if ( $theme_option['flagship_sub_quicklinks']  == '1' ) { restore_current_blog(); }
 		 ?>
-
+		</nav>
 		<!-- Footer Links -->
-		<div class="medium-6 columns" aria-labelledby="menu-footer-links">
+		<div class="medium-6 columns" role="navigation" aria-labelledby="menu-footer-links">
 			<ul id="menu-footer-links" class="inline-list hide-for-small-only" role="menu">
 				<li role="menuitem"><a href="<?php echo get_site_url(); ?>/sitemap">Sitemap</a></li>
 				<li role="menuitem"><a href="<?php echo get_site_url(); ?>/about/jobs/">Employment</a></li>
@@ -39,19 +38,17 @@
 				<a href="https://www.youtube.com/user/jhuksas" title="YouTube"><span class="fa fa-youtube-square fa-3x"></span><span class="screen-reader-text">YouTube</span></a>
 			</div>
 		</div>
-
-		<!-- Copyright and Address -->
-		<div class="row" id="copyright" role="contentinfo">
-  			<p>&copy; <?php print date('Y'); ?> Johns Hopkins University, <?php echo $theme_option['flagship_sub_copyright'];?></p>
-  		</div>
-  		<div class="row">
-	  		<div class="small-4 small-centered columns">
-  				<a href="http://www.jhu.edu"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/university.jpg" alt="Johns Hopkins University logo"/></a>
-  			</div>
-  		</div>
-
+	</div>
+	<!-- Copyright and Address -->
+	<div class="row" id="copyright" role="contentinfo">
+  		<p>&copy; <?php print date('Y'); ?> Johns Hopkins University, <?php echo $theme_option['flagship_sub_copyright'];?></p>
   	</div>
-  </footer>
+  	<div class="row">
+	  	<div class="small-12 small-centered medium-4 columns">
+  				<a href="http://www.jhu.edu" title="Johns Hopkins University"><img src="<?php echo get_template_directory_uri() ?>/assets/images/university.jpg" / alt="Johns Hopkins University logo"></a>
+  		</div>
+  	</div>
+</footer>
 
   <?php //Call all the javascript
   		get_template_part('parts', 'script-initiators');
