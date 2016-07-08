927d1d86334817ac75cf89f917560c045ab78d93
ksascomm <ksasweb@jhu.edu>
2016-07-08 16:01:00 -0400

    custom sitemap.php template

---
+++ template-sitemap.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Sitemap Listing (Menus before Content)
+*/
+?>	
+
+<?php get_header(); ?>
+<div class="row sidebar_bg radius10" id="page">
+	<div class="small-12 large-8 columns wrapper radius-left offset-topgutter">	
+		<?php locate_template('parts-nav-breadcrumbs.php', true, false); ?>	
+		<main class="content" role="main">
+			<?php wp_nav_menu( array( 
+				'theme_location' => 'main_nav', 
+				'fallback_cb' => 'foundation_page_menu', 
+				) ); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1 class="page-title"><?php the_title();?></h1>
+				<?php the_content(); ?>
+			<?php endwhile; endif; ?>	
+		</main>
+	</div>	<!-- End main content (left) section -->
+<?php locate_template('parts-sidebar.php', true, false); ?>
+</div> <!-- End #landing -->
+<?php get_footer(); ?>
